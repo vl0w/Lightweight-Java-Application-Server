926a9c4fd9fc3fa39cb6456146f8a986c67df96c
Jonas Hansen <jonas.hansen@bluewin.ch>
2013-03-07 21:19:43 +0100

    Build the exception's message differently when only one exception was thrown

--- src/ljas/commons/application/client/ClientApplicationException.java
+++ src/ljas/commons/application/client/ClientApplicationException.java
@@ -25,16 +25,20 @@ public class ClientApplicationException extends Exception {
 
 	private static String buildMessageForMultipleTaskExceptions(
 			List<TaskException> taskExceptions) {
-		final String LB = "\n";
-		StringBuilder sb = new StringBuilder();
+		if (taskExceptions.size() == 1) {
+			return taskExceptions.get(0).getMessage();
+		} else {
+			final String LB = "\n";
+			StringBuilder sb = new StringBuilder();
 
-		sb.append("The following exceptions occured while executing a task:");
-		sb.append(LB);
-		for (TaskException taskException : taskExceptions) {
-			sb.append(taskException.getMessage());
+			sb.append("The following exceptions occured while executing a task:");
 			sb.append(LB);
-		}
+			for (TaskException taskException : taskExceptions) {
+				sb.append(taskException.getMessage());
+				sb.append(LB);
+			}
 
-		return sb.toString();
+			return sb.toString();
+		}
 	}
 }
