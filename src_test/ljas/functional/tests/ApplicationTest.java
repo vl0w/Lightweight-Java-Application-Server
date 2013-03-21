db128d19db0a39c5d0eea599cb68533643753801
Jonas Hansen <jonas.hansen@bluewin.ch>
2013-03-21 20:14:23 +0100

    Add functional test for application proxy

---
+++ src_test/ljas/functional/tests/ApplicationTest.java
@@ -0,0 +1,17 @@
+package ljas.functional.tests;
+
+import static org.junit.Assert.assertEquals;
+import ljas.client.Client;
+import ljas.functional.ServerTestCase;
+import ljas.functional.application.TestApplication;
+
+import org.junit.Test;
+
+public class ApplicationTest extends ServerTestCase {
+	@Test
+	public void testApplication() throws Exception {
+		Client client = createAndConnectClient();
+		TestApplication application = (TestApplication) client.getApplication();
+		assertEquals(15, application.getSum(10, 5));
+	}
+}
