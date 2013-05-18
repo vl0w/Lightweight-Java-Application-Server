84fbdf4d45512f2c34f2d709460b6091be56cbf3
Jonas Hansen <jonas.hansen@bluewin.ch>
2013-05-18 19:44:53 +0200

    Removed unused constructors

--- src/ljas/exception/DisconnectException.java
+++ src/ljas/exception/DisconnectException.java
@@ -2,26 +2,10 @@ package ljas.exception;
 
 public class DisconnectException extends Exception {
 
-	private static final long serialVersionUID = 4731954858525396395L;
-
-	public DisconnectException() {
-	}
-
-	public DisconnectException(String message) {
-		super(message);
-	}
+	private static final long serialVersionUID = -2174545773593395383L;
 
 	public DisconnectException(Throwable cause) {
 		super(cause);
-	}
-
-	public DisconnectException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
-	public DisconnectException(String message, Throwable cause,
-			boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
 	}
 
 }
