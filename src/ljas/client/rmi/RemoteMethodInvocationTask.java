8ed5f89fccd32bd2e8128c7a5fb30dd8e13f88c9
Jonas Hansen <jonas.hansen@bluewin.ch>
2013-05-18 23:13:59 +0200

    Copy array in method to prevent changes from outside (array won't get stored directly)

--- src/ljas/client/rmi/RemoteMethodInvocationTask.java
+++ src/ljas/client/rmi/RemoteMethodInvocationTask.java
@@ -1,6 +1,7 @@
 package ljas.client.rmi;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 import ljas.application.Application;
 import ljas.client.Client;
@@ -25,8 +26,14 @@ public class RemoteMethodInvocationTask extends SimpleClientToServerTask {
 			Class<?>[] parameterTypes, Object[] args) {
 		super(client);
 		this.methodName = methodName;
-		this.parameterTypes = parameterTypes;
-		this.args = args;
+		if (parameterTypes != null) {
+			this.parameterTypes = Arrays.copyOf(parameterTypes,
+					parameterTypes.length);
+		}
+		if (args != null) {
+			this.args = Arrays.copyOf(args, args.length);
+			;
+		}
 	}
 
 	public Object getReturnValue() {
