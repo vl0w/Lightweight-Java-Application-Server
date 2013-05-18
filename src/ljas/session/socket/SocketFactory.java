e69d7a207f382adcc71e72617f1e5289823a935d
Jonas Hansen <jonas.hansen@bluewin.ch>
2013-05-18 18:10:46 +0200

    Do not use unnecessary exception

--- src/ljas/session/socket/SocketFactory.java
+++ src/ljas/session/socket/SocketFactory.java
@@ -2,14 +2,12 @@ package ljas.session.socket;
 
 import java.io.IOException;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 import ljas.session.Address;
 
 class SocketFactory {
 
-	public Socket createSocket(Address address) throws IOException,
-			UnknownHostException {
+	public Socket createSocket(Address address) throws IOException {
 		return new Socket(address.getHost(), address.getPort());
 	}
 }
